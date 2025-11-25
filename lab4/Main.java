811a2fd32ee3221e7203ed20c1d2b61e5e3e7c60
Student <vadim.cotoman@iis.utm.md>
2025-11-26 01:25:27 +0200

    corectat

---
+++ lab4/Main.java
@@ -0,0 +1,198 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        ProducerConsumerGUI gui = new ProducerConsumerGUI();
+
+        int D = 5;
+        int Y = 4;
+        int Z = 2;
+        int X = 3;
+
+        Depozit depozit = new Depozit(D, Y, Z, gui);
+
+        Producer p1 = new Producer(1, depozit, gui);
+        Producer p2 = new Producer(2, depozit, gui);
+        Producer p3 = new Producer(3, depozit, gui);
+
+        Consumer c1 = new Consumer(1, depozit, gui);
+        Consumer c2 = new Consumer(2, depozit, gui);
+        Consumer c3 = new Consumer(3, depozit, gui);
+        Consumer c4 = new Consumer(4, depozit, gui);
+
+        p1.start();
+        p2.start();
+        p3.start();
+
+        c1.start();
+        c2.start();
+        c3.start();
+        c4.start();
+    }
+}
+
+class Producer extends Thread {
+    private int id;
+    private Depozit depozit;
+    private ProducerConsumerGUI gui;
+    private Random random = new Random();
+
+    public Producer(int id, Depozit depozit, ProducerConsumerGUI gui) {
+        this.id = id;
+        this.depozit = depozit;
+        this.gui = gui;
+    }
+
+    // Metoda pune() pentru Producer
+    public void pune(int nr) {
+        depozit.pune(nr);
+        gui.log("Producător " + id + " a pus produs: " + nr);
+    }
+
+    public void run() {
+        while (true) {
+            if (depozit.productieCompleta()) {
+                gui.log("Producător " + id + " s-a oprit - productie completa");
+                return;
+            }
+
+            int nr1 = genereazaNumarImpar();
+            int nr2 = genereazaNumarImpar();
+
+            gui.log("Producător " + id + " a generat: " + nr1 + " și " + nr2);
+
+            pune(nr1);
+            pune(nr2);
+
+            try {
+                Thread.sleep(random.nextInt(300) + 100);
+            } catch (InterruptedException e) {}
+        }
+    }
+
+    private int genereazaNumarImpar() {
+        int nr = random.nextInt(50) + 1;
+        if (nr % 2 == 0) {
+            nr++;
+        }
+        return nr;
+    }
+}
+
+class Consumer extends Thread {
+    private int id;
+    private Depozit depozit;
+    private ProducerConsumerGUI gui;
+    private Random random = new Random();
+    private int consumate = 0;
+
+    public Consumer(int id, Depozit depozit, ProducerConsumerGUI gui) {
+        this.id = id;
+        this.depozit = depozit;
+        this.gui = gui;
+    }
+
+    // Metoda ia() pentru Consumer
+    public int ia() {
+        int nr = depozit.ia();
+
+        if (nr == -1) {
+            gui.log("Consumator " + id + " s-a oprit - nu mai sunt produse. A consumat: " + consumate + "/" + depozit.Z);
+            return -1;
+        }
+
+        consumate++;
+        gui.log("Consumator " + id + " a consumat: " + nr + " (total consumate: " + consumate + "/" + depozit.Z + ")");
+        return nr;
+    }
+
+    public void run() {
+        while (consumate < depozit.Z) {
+            int nr = ia();
+
+            if (nr == -1) {
+                return;
+            }
+
+            try {
+                Thread.sleep(random.nextInt(200) + 100);
+            } catch (InterruptedException e) {}
+        }
+
+        gui.log("Consumator " + id + " a terminat! A consumat total: " + consumate + " produse");
+    }
+}
+
+class Depozit {
+    private int[] buffer;
+    int D;
+    int Z;
+    private ProducerConsumerGUI gui;
+
+    private int pozitieScrie = 0;
+    private int pozitieCiteste = 0;
+    private int cantitate = 0;
+
+    private int totalProduse = 0;
+    private int totalConsumate = 0;
+    private int totalNecesar;
+
+    public Depozit(int D, int Y, int Z, ProducerConsumerGUI gui) {
+        this.D = D;
+        this.Z = Z;
+        this.buffer = new int[D];
+        this.totalNecesar = Y * Z;
+        this.gui = gui;
+
+        gui.log("=== CONFIGURARE ===");
+        gui.log("Depozit: " + D);
+        gui.log("Consumatori: " + Y);
+        gui.log("Produse/consumator: " + Z);
+        gui.log("Total necesar: " + totalNecesar);
+        gui.log("===================");
+    }
+
+    public synchronized boolean productieCompleta() {
+        return totalProduse >= totalNecesar;
+    }
+
+    public synchronized void pune(int nr) {
+        while (cantitate >= D) {
+            try {
+                wait();
+            } catch (InterruptedException e) {}
+        }
+
+        if (totalProduse >= totalNecesar) {
+            notifyAll();
+            return;
+        }
+
+        buffer[pozitieScrie] = nr;
+        pozitieScrie = (pozitieScrie + 1) % D;
+        cantitate++;
+        totalProduse++;
+
+        notifyAll();
+    }
+
+    public synchronized int ia() {
+        while (cantitate == 0) {
+            if (totalProduse >= totalNecesar && totalConsumate >= totalNecesar) {
+                return -1;
+            }
+
+            try {
+                wait();
+            } catch (InterruptedException e) {}
+        }
+
+        int nr = buffer[pozitieCiteste];
+        pozitieCiteste = (pozitieCiteste + 1) % D;
+        cantitate--;
+        totalConsumate++;
+
+        notifyAll();
+        return nr;
+    }
+}
--- lab4/ProducerConsumerGUI.java
+++ lab4/ProducerConsumerGUI.java
@@ -2,18 +2,17 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ProducerConsumerGUI extends JFrame {
-
     private JTextArea textArea;
 
     public ProducerConsumerGUI() {
-        setTitle("Producer - Consumer Monitor");
-        setSize(700, 500);
+        setTitle("Monitor Producător-Consumator");
+        setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
         textArea = new JTextArea();
         textArea.setEditable(false);
-        textArea.setFont(new Font("Monospaced", Font.PLAIN, 14));
+        textArea.setFont(new Font("Monospaced", Font.PLAIN, 13));
 
         JScrollPane scrollPane = new JScrollPane(textArea);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
@@ -26,7 +25,7 @@ public class ProducerConsumerGUI extends JFrame {
     public synchronized void log(String message) {
         SwingUtilities.invokeLater(() -> {
             textArea.append(message + "\n");
-            textArea.setCaretPosition(textArea.getDocument().getLength()); // auto-scroll
+            textArea.setCaretPosition(textArea.getDocument().getLength());
         });
     }
 }
